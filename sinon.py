248984b0c5dc9c2e19cf6adab96e9936a83622cd
Puppet <titushein@web.de>
2024-07-06 22:43:52 +0200

    fix for the follower count but its not working

--- sinon.py
+++ sinon.py
@@ -18,6 +18,7 @@ class MyClient(discord.Client):
     def __init__(self):
         super().__init__(intents=discord.Intents.default())
         self.channel = None
+        self.reported_streams = {}  # Cache for reported streams
 
     async def on_ready(self):
         print(f'Logged in as {self.user}')
@@ -25,71 +26,109 @@ class MyClient(discord.Client):
         check_streams.start()  # Start the task
 
     async def get_twitch_streams(self, category_name):
-        game_url = f"https://api.twitch.tv/helix/games?name={category_name}"
-        headers = {
-            'Client-ID': TWITCH_CLIENT_ID,
-            'Authorization': f'Bearer {TWITCH_ACCESS_TOKEN}'
-        }
-        game_response = requests.get(game_url, headers=headers).json()
-        game_id = game_response['data'][0]['id']
+        try:
+            game_url = f"https://api.twitch.tv/helix/games?name={category_name}"
+            headers = {
+                'Client-ID': TWITCH_CLIENT_ID,
+                'Authorization': f'Bearer {TWITCH_ACCESS_TOKEN}'
+            }
+            game_response = requests.get(game_url, headers=headers).json()
+            game_id = game_response['data'][0]['id']
 
-        url = f"https://api.twitch.tv/helix/streams?game_id={game_id}"
-        response = requests.get(url, headers=headers)
-        return response.json()
+            url = f"https://api.twitch.tv/helix/streams?game_id={game_id}"
+            response = requests.get(url, headers=headers)
+            return response.json()
+        except Exception as e:
+            print(f"Error fetching streams: {e}")
+            return {'data': []}
 
+    async def get_user_info(self, user_id):
+        try:
+            url = f"https://api.twitch.tv/helix/users?id={user_id}"
+            headers = {
+                'Client-ID': TWITCH_CLIENT_ID,
+                'Authorization': f'Bearer {TWITCH_ACCESS_TOKEN}'
+            }
+            response = requests.get(url, headers=headers).json()
+
+            user_data = response.get('data', [])
+            if user_data:
+                user_info = user_data[0]
+                followers_url = f"https://api.twitch.tv/helix/users/follows?to_id={user_id}&from_id={user_id}"
+                followers_response = requests.get(followers_url, headers=headers).json()
+
+                if followers_response.get('total', 0) > 0:
+                    follower_count = followers_response['total']
+                else:
+                    follower_count = 0
+
+                return {
+                    'id': user_info.get('id'),
+                    'name': user_info.get('display_name'),
+                    'profile_image_url': user_info.get('profile_image_url'),
+                    'description': user_info.get('description'),
+                    'follower_count': follower_count
+                }
+            else:
+                return {}
+        except Exception as e:
+            print(f"Error fetching user info: {e}")
+            return {}
 
 @tasks.loop(minutes=2.5)
 async def check_streams():
-    channel = client.get_channel(CHANNEL_ID)
     streams = await client.get_twitch_streams(CATEGORY_NAME)
 
-    # Check if each stream is already reported
+    for stream_id, reported_stream in list(client.reported_streams.items()):
+        if stream_id not in [stream['id'] for stream in streams['data']]:
+            await reported_stream['message'].delete()
+            del client.reported_streams[stream_id]
+
     for stream in streams['data']:
         stream_title = stream['title']
         stream_url = f"https://www.twitch.tv/{stream['user_name']}"
+        stream_id = stream['id']
+        user_id = stream['user_id']
 
-        # Check if the stream is already reported
-        already_reported = False
-        async for message in channel.history(limit=100):
-            if message.embeds and message.embeds[
-                    0].title == stream_title and message.embeds[
-                        0].url == stream_url:
-                already_reported = True
-                break
+        user_info = await client.get_user_info(user_id)
+        follower_count = user_info.get('view_count', 0)
+        description = user_info.get('description', '')
 
-        if already_reported:
-            # Update the viewer count for the already reported stream
-            embed = discord.Embed(
-                title=stream_title,
-                url=stream_url,
-                description=
-                f"{stream['user_name']} is live streaming {CATEGORY_NAME}",
-                color=discord.Color(0x9900ff))
-            embed.set_author(name=stream['user_name'], url=stream_url)
-            embed.add_field(name="Viewers", value=stream['viewer_count'])
-            embed.set_thumbnail(url=stream['thumbnail_url'])
-            embed.set_footer(text="Sinon - Made by Puppetino")
+        if stream_id in client.reported_streams:
+            message = client.reported_streams[stream_id]['message']
 
-            # Find the message with the already reported stream
-            async for message in channel.history(limit=100):
-                if message.embeds and message.embeds[
-                        0].title == stream_title and message.embeds[
-                            0].url == stream_url:
-                    await message.edit(embed=embed)
-                    break
+            if stream['type'] != 'live':
+                await message.delete()
+                del client.reported_streams[stream_id]
+            else:
+                embed = discord.Embed(
+                    title=stream_title,
+                    url=stream_url,
+                    description=f"{stream['user_name']} is live streaming {CATEGORY_NAME} on Twitch.\n\n{description}",
+                    color=discord.Color(0x9900ff)
+                )
+                embed.set_author(name=stream['user_name'], url=stream_url, icon_url=user_info.get('profile_image_url'))
+                embed.add_field(name="Viewers", value=stream['viewer_count'])
+                embed.add_field(name="Followers", value=user_info['follower_count'], inline=True)
+                embed.set_thumbnail(url=stream['thumbnail_url'])
+                embed.set_footer(text="Sinon - Made by Puppetino")
+
+                await message.edit(embed=embed)
         else:
-            # Report the new stream
             embed = discord.Embed(
                 title=stream_title,
                 url=stream_url,
-                description=
-                f"{stream['user_name']} is live streaming {CATEGORY_NAME}",
-                color=discord.Color(0x9900ff))
-            embed.set_author(name=stream['user_name'], url=stream_url)
+                description=f"{stream['user_name']} is live streaming {CATEGORY_NAME} on Twitch.\n\n{description}",
+                color=discord.Color(0x9900ff)
+            )
+            embed.set_author(name=stream['user_name'], url=stream_url, icon_url=user_info.get('profile_image_url'))
             embed.add_field(name="Viewers", value=stream['viewer_count'])
+            embed.add_field(name="Followers", value=user_info['follower_count'], inline=True)
             embed.set_thumbnail(url=stream['thumbnail_url'])
             embed.set_footer(text="Sinon - Made by Puppetino")
-            await channel.send(embed=embed)
+
+            message = await client.channel.send(embed=embed)
+            client.reported_streams[stream_id] = {'message': message}
 
 
 client = MyClient()
